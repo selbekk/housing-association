c356f6c5edc5e3d34491a37f28eb78e791a07dba
selbekk <kristofer.selbekk@bekk.no>
2015-02-02 20:32:00 +0100

    Serve static index page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(3000, function() {
