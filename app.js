faf432d4257c104e62ace8a88d4eaca760246040
selbekk <kristofer.selbekk@bekk.no>
2015-02-02 20:36:41 +0100

    serve static files through express' included middleware

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
+// Serve static files from the public directory
+app.use(express.static('public'))
 
 app.listen(3000, function() {
     console.log('Listening for connections at localhost:3000...');
