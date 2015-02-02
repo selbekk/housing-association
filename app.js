633b6ef07cb276acb8810277ac8d7a0996092579
selbekk <kristofer.selbekk@bekk.no>
2015-02-02 22:57:39 +0100

    Refactor API endpoints to their own router

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
+var apiRouter = require('./api/router.js');
+
 // Serve static files from the public directory
 app.use(express.static('public'))
 
+// Serve API resources
+app.use('/api', apiRouter),
+
+// Start server
 app.listen(3000, function() {
     console.log('Listening for connections at localhost:3000...');
 });
