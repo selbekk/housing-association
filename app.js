6d7adbfcbc677a7892ca0f95f39c554f948e24c7
selbekk <kristofer.selbekk@bekk.no>
2015-02-04 19:22:57 +0100

    Add access logging

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express = require('express');
-var app = express();
+var morgan = require('morgan');
+var fs = require('fs');
 
+var settings = require('./util/settings');
 var organizationsRouter = require('./organizations/organization-router');
 
-// Serve static files from the public directory
+// Set up express
+var app = express();
+
+// Serve static files
 app.use(express.static('public'));
 
+// Set up access logger
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+app.use(morgan(settings.logLevel, { stream: accessLogStream }));
+
+// Log to stdout as well, if in development
+if(settings.env === 'dev') {
+    app.use(morgan(settings.logLevel));
+}
+
+// Set up routes
 app.use('/organizations', organizationsRouter);
 
 // Start server
