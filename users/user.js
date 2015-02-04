8383952625eeaa7468e600376ed644758ea98435
selbekk <kristofer.selbekk@bekk.no>
2015-02-04 20:16:32 +0100

    add isValid() method to users (including user validation)

--- users/user.js
+++ users/user.js
@@ -1,5 +1,11 @@
+var validator = require('validator');
+
 module.exports = function(opts) {
     this.username = opts.username;
     this.email = opts.email;
     this.phone = opts.phone;
+
+    this.isValid = function() {
+        return this.username && validator.isEmail(this.email);
+    }
 }
